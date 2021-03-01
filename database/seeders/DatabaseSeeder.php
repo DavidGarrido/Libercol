b94d178d5da91323013a25223b9a836dddf09382
www.davidalexander@gmail.com <www.davidalexander@gmail.com>
2021-03-01 17:36:35 -0500

    antes del build

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            DepartamentSeeder::class,
+            MunicipalitySeeder::class,
+            CompanySeeder::class,
+            IndustrySeeder::class,
+            FiscalSeeder::class,
+            TaxSeeder::class,
+            AddressSeeder::class,
+            ContactSeeder::class,
+            FileSeeder::class,
+            PointSeeder::class,
+            InventarySeeder::class,
+            TicketSeeder::class,
+            AssetSeeder::class,
+            FeatureSeeder::class,
+            ColorSeeder::class,
+            MaterialSeeder::class,
+            ListsaleSeeder::class,
+            ProductSeeder::class,
+            ExtraSeeder::class,
+            WallettypeSeeder::class,
+            WalletSeeder::class,
+            WalletmovimentSeeder::class,
+            BankSeeder::class,
+            UserdataSeeder::class,
+            ClientSeeder::class,
+            ScoreSeeder::class,
+            CategorySeeder::class,
+            RoleSeeder::class,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
-use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ColorController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DepartamentController;
+use App\Http\Controllers\InventaryController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\MunicipalityController;
+use App\Http\Controllers\PointController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -35,8 +46,19 @@ Route::group(['middleware' => ['auth:sanctum','verified']], function() {
     Route::get('/dashboard', function() {
         return Inertia::render('Dashboard');
     })->name('dashboard');
-
-    Route::resource('{rol}/empresa', EmpresaController::class)->names('empresa');
+    
+    
+    Route::resource('company', CompanyController::class)->names('companies');    
+    Route::resource('wallet', WalletController::class)->names('wallets');    
+    Route::resource('color', ColorController::class)->names('colors');    
+    Route::resource('material', MaterialController::class)->names('materials');    
+    Route::resource('municipality', MunicipalityController::class)->names('municipalities');    
+    Route::resource('departament', DepartamentController::class)->names('departaments');    
+    
+    Route::resource('{rol}ticket', TicketController::class)->names('tickets');    
+    Route::resource('{rol}inventary', InventaryController::class)->names('inventaries');    
+    Route::resource('{rol}asset', AssetController::class)->names('assets');    
+    Route::resource('{rol}client', ClientController::class)->names('clients');    
+    Route::resource('{rol}point', PointController::class)->names('points');    
+    Route::resource('{rol}category', CategoryController::class)->names('categories');
 });
-
-
